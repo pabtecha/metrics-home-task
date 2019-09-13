6f3e09f6fdef0f17803a5ce2b7fe6db491a6753c
Nico Terevinto <pabtecha@gmail.com>
2019-09-13 18:34:57 +0200

    feat(metrics): create viewset with basic list

--- metrics/views.py
+++ metrics/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+
+class MetricsViewSet(ViewSet):
+
+    def list(self, request):
+        return Response(status=status.HTTP_200_OK)
