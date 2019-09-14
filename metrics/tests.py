14e9ff46c2ff48a0c29544f09606aceb3abbcfd1
Nico Terevinto <pabtecha@gmail.com>
2019-09-14 12:12:40 +0200

    refactor(metrics): refactor tests to set up class data

--- metrics/tests.py
+++ metrics/tests.py
@@ -46,62 +46,37 @@ metrics_json = [
 
 
 class MetricsViewSetTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        metrics = [Metrics(**row) for row in metrics_json]
+        Metrics.objects.bulk_create(metrics)
 
     def test_metrics_returns_200(self):
         res = self.client.get('/metrics')
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    def test_metrics_returns_elements(self):
-        metric = Metrics(**metrics_json[0])
-        metric.save()
-
+    def test_metrics_returns_all_elements(self):
         res = self.client.get('/metrics')
-        self.assertEqual(len(res.json()["data"]), 1)
+        self.assertEqual(len(res.json()["data"]), len(metrics_json))
 
     def test_metrics_with_field_filter_returns_filtered_elements(self):
-        metric = Metrics(**metrics_json[0])
-        metric.save()
-        metric2 = Metrics(**metrics_json[1])
-        metric2.save()
-
-        res = self.client.get('/metrics', {"country": "US"})
+        res = self.client.get('/metrics', {"country": "ES"})
         self.assertEqual(len(res.json()["data"]), 1)
 
     def test_metrics_with_multiple_fields_filter_returns_filtered_elements(self):
-        metric = Metrics(**metrics_json[0])
-        metric.save()
-        metric2 = Metrics(**metrics_json[1])
-        metric2.save()
-        metric3 = Metrics(**metrics_json[2])
-        metric3.save()
-
         params = {"country": "US", "os": "android", "channels": "unityads"}
 
         res = self.client.get('/metrics', params)
         self.assertEqual(len(res.json()["data"]), 1)
 
     def test_metrics_with_same_day_date_range_filter_returns_filtered_elements(self):
-        metric = Metrics(**metrics_json[0])
-        metric.save()
-        metric2 = Metrics(**metrics_json[1])
-        metric2.save()
-        metric3 = Metrics(**metrics_json[2])
-        metric3.save()
-
         params = {"date_from": "2017-08-17", "date_to": "2017-08-17"}
 
         res = self.client.get('/metrics', params)
         self.assertEqual(len(res.json()["data"]), 1)
 
     def test_metrics_with_date_range_filter_returns_filtered_elements(self):
-        metric = Metrics(**metrics_json[0])
-        metric.save()
-        metric2 = Metrics(**metrics_json[1])
-        metric2.save()
-        metric3 = Metrics(**metrics_json[2])
-        metric3.save()
-
-        params = {"date_from": "2017-05-15", "date_to": "2017-08-17"}
+        params = {"date_from": "2017-05-15", "date_to": "2017-05-17"}
 
         res = self.client.get('/metrics', params)
         self.assertEqual(len(res.json()["data"]), 2)
