326821a472d13034ecaea82952f759ebd9613401
Nico Terevinto <pabtecha@gmail.com>
2019-09-14 13:05:00 +0200

    feat(metrics): add group by query

--- metrics/serializers.py
+++ metrics/serializers.py
@@ -7,3 +7,9 @@ class MetricsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Metrics
         fields = '__all__'
+        extra_kwargs = {
+            'date': {'required': False},
+            'country': {'required': False},
+            'channel': {'required': False},
+            'os': {'required': False},
+        }
--- metrics/views.py
+++ metrics/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -11,5 +12,13 @@ class MetricsViewSet(ViewSet):
 
     def list(self, request):
         f = MetricsFilter(request.GET, queryset=Metrics.objects.all())
-        response_data = MetricsSerializer(instance=f.qs, many=True).data
+        metrics = f.qs
+        gruop_by = request.GET.get('group_by')
+        if gruop_by:
+            metrics = f.qs.values(gruop_by).annotate(impressions=Sum('impressions'),
+                                                     clicks=Sum('clicks'),
+                                                     installs=Sum('installs'),
+                                                     spend=Sum('spend'),
+                                                     revenue=Sum('revenue'))
+        response_data = MetricsSerializer(instance=metrics, many=True).data
         return Response({"data": response_data}, status=status.HTTP_200_OK)
