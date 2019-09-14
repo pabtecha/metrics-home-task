ab02e880b7fe66187cb2787203a81c042df8ed78
Nico Terevinto <pabtecha@gmail.com>
2019-09-14 12:05:36 +0200

    feat(metrics): add filter by date range with inclusive dates

--- metrics/filters.py
+++ metrics/filters.py
@@ -4,6 +4,9 @@ from metrics.models import Metrics
 
 
 class MetricsFilter(django_filters.FilterSet):
+    date_from = django_filters.DateFilter(field_name='date', lookup_expr='gte')
+    date_to = django_filters.DateFilter(field_name='date', lookup_expr='lte')
+
     class Meta:
         model = Metrics
         fields = ['country', "os", "channel"]
