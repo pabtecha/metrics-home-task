a7faa646b548afe8b6b2a22045b3ae21053c35f2
Nico Terevinto <pabtecha@gmail.com>
2019-09-14 11:26:40 +0200

    test(metrics): add test filter by multiple fields

--- metrics/tests.py
+++ metrics/tests.py
@@ -23,7 +23,25 @@ metrics_json = [
      'clicks': '494',
      'installs': '76',
      'spend': '148.2',
-     'revenue': '149.04'}
+     'revenue': '149.04'},
+    {'date': '2017-05-17',
+     'channel': 'adcolony',
+     'country': 'US',
+     'os': 'ios',
+     'impressions': '19887',
+     'clicks': '494',
+     'installs': '76',
+     'spend': '148.2',
+     'revenue': '149.04'},
+    {'date': '2017-05-17',
+     'channel': 'unityads',
+     'country': 'US',
+     'os': 'ios',
+     'impressions': '19887',
+     'clicks': '494',
+     'installs': '76',
+     'spend': '148.2',
+     'revenue': '149.04'},
 ]
 
 
@@ -46,5 +64,18 @@ class MetricsViewSetTest(TestCase):
         metric2 = Metrics(**metrics_json[1])
         metric2.save()
 
-        res = self.client.get('/metrics', params={"country": "US"})
+        res = self.client.get('/metrics', {"country": "US"})
+        self.assertEqual(len(res.json()["data"]), 1)
+
+    def test_metrics_with_multiple_fields_filter_returns_filtered_elements(self):
+        metric = Metrics(**metrics_json[0])
+        metric.save()
+        metric2 = Metrics(**metrics_json[1])
+        metric2.save()
+        metric3 = Metrics(**metrics_json[2])
+        metric3.save()
+
+        params = {"country": "US", "os": "android", "channels": "unityads"}
+
+        res = self.client.get('/metrics', params)
         self.assertEqual(len(res.json()["data"]), 1)
