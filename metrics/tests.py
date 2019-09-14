0695727917c975952b7520d6137f763efd9bfd18
Nico Terevinto <pabtecha@gmail.com>
2019-09-14 13:04:30 +0200

    test(metrics): add group by single field test

--- metrics/tests.py
+++ metrics/tests.py
@@ -46,37 +46,43 @@ metrics_json = [
 
 
 class MetricsViewSetTest(TestCase):
+    metrics_url = '/metrics'
     @classmethod
     def setUpTestData(cls):
         metrics = [Metrics(**row) for row in metrics_json]
         Metrics.objects.bulk_create(metrics)
 
-    def test_metrics_returns_200(self):
-        res = self.client.get('/metrics')
+    def test_list_returns_200(self):
+        res = self.client.get(self.metrics_url)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    def test_metrics_returns_all_elements(self):
-        res = self.client.get('/metrics')
+    def test_list_returns_all_elements(self):
+        res = self.client.get(self.metrics_url)
         self.assertEqual(len(res.json()["data"]), len(metrics_json))
 
-    def test_metrics_with_field_filter_returns_filtered_elements(self):
-        res = self.client.get('/metrics', {"country": "ES"})
+    def test_list_with_field_filter_returns_filtered_elements(self):
+        res = self.client.get(self.metrics_url, {"country": "ES"})
         self.assertEqual(len(res.json()["data"]), 1)
 
-    def test_metrics_with_multiple_fields_filter_returns_filtered_elements(self):
+    def test_list_with_multiple_fields_filter_returns_filtered_elements(self):
         params = {"country": "US", "os": "android", "channels": "unityads"}
 
-        res = self.client.get('/metrics', params)
+        res = self.client.get(self.metrics_url, params)
         self.assertEqual(len(res.json()["data"]), 1)
 
-    def test_metrics_with_same_day_date_range_filter_returns_filtered_elements(self):
+    def test_list_with_same_day_date_range_filter_returns_filtered_elements(self):
         params = {"date_from": "2017-08-17", "date_to": "2017-08-17"}
 
-        res = self.client.get('/metrics', params)
+        res = self.client.get(self.metrics_url, params)
         self.assertEqual(len(res.json()["data"]), 1)
 
-    def test_metrics_with_date_range_filter_returns_filtered_elements(self):
+    def test_list_with_date_range_filter_returns_filtered_elements(self):
         params = {"date_from": "2017-05-15", "date_to": "2017-05-17"}
 
-        res = self.client.get('/metrics', params)
+        res = self.client.get(self.metrics_url, params)
+        self.assertEqual(len(res.json()["data"]), 2)
+
+    def test_list_with_group_by_single_field_returns_grouped_elements(self):
+        params = {"group_by": "country"}
+        res = self.client.get(self.metrics_url, params)
         self.assertEqual(len(res.json()["data"]), 2)
